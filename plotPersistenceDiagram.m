ca6b604187ad924e27ea8fee4702ccb8e6419c2e
Marko <mbudisic@clarkson.edu>
2021-02-25 23:08:11 -0500

    Added options to plot birth-death or birth/death-persistence

--- plotPersistenceDiagram.m
+++ plotPersistenceDiagram.m
@@ -1,10 +1,10 @@
-function h = plotPersistenceDiagram( barcode )
+function h = plotPersistenceDiagram( barcode, opts )
 % PLOTPERSISTENCEDIAGRAM Plots a barcode.
 % h = plotPersistenceDiagram( barcode )
 %
 % Input:
 %
-%      barcode - an N x 2 matrix of start/stop values representing each 
+%      barcode - an N x 2 matrix of start/stop values representing each
 %                bar
 %
 % Output:
@@ -18,8 +18,9 @@ function h = plotPersistenceDiagram( barcode )
 %   [h.LineWidth] = deal( 2 );
 
 arguments
-
+    
     barcode (:,:) {mustBeNonempty}
+    opts.type {mustBeMember(opts.type,{'birthdeath','birthpersistence','deathpersistence'})} = 'rips'
     
 end
 
@@ -27,12 +28,29 @@ end
 isPersistent = isinf(barcode(:,2));
 
 % set the "inf" values to twice the max;
-barcode(isPersistent, 2) = 2*max( barcode(~isPersistent,2) );
+%barcode(isPersistent, 2) = 2*max( barcode(~isPersistent,2) );
+
+switch(opts.type)
+    case 'birthdeath'
+        xvec = @(m)m(:,1); xtext = 'Birth';
+        yvec = @(m)m(:,2); ytext = 'Death';
+        
+    case 'birthpersistence'
+        xvec = @(m)m(:,1); xtext = 'Birth';
+        yvec = @(m)diff(m,1,2); ytext = 'Persistence';
+        
+        
+    case 'deathpersistence'
+        xvec = @(m)m(:,2); xtext = 'Death';
+        yvec = @(m)diff(m,1,2); ytext = 'Persistence';
+    
+end
 
 
-h1 = plot( barcode(~isPersistent,1), barcode(~isPersistent,2), '.', 'MarkerSize',10 ); hold all;
-h2 = plot( barcode(isPersistent,1), barcode(isPersistent,2), 'x', 'MarkerSize',8, 'Color',h1.Color );
+h1 = plot( xvec( barcode(~isPersistent,:) ), yvec( barcode(~isPersistent,:) ), '.', 'MarkerSize',10 ); hold all;
+h2 = plot( xvec( barcode(isPersistent,:) ), yvec( barcode(isPersistent,:) ), 'x', 'MarkerSize',8, 'Color',h1.Color );
 hold off;
+xlabel(xtext); ylabel(ytext);
 
 h = [h1,h2];
 end
